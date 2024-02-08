05429a170d4fdde01b812335291b4769c0a885f2
“Cardinalempath” <cardinaljamez@gmail.com>
2024-02-08 14:43:50 +0200

    first work

--- run.py
+++ run.py
@@ -1,12 +1,40 @@
-def details():
-    input("Enter your name")
-    input("Enter your Surname")
-    input("Enter your email")
+def hash_password(password):
+    hashed_password = ''
+    for i in range(len(password)):
+        if i % 2 == 1:  
+            hashed_password += '#'
+        else:
+            hashed_password += password[i]
+    return hashed_password
 
-print(details)
+def details():
+    name = input("Enter your name: ")
+    surname = input("Enter your Surname: ")
+    email = input("Enter your email: ")
+
+    return name, surname, email
 
 def access():
-    input("Enter password")
-password =  input("Re- Enter password")
+    entered_password = input("Enter password: ")
+    reentered_password = input("Re-Enter password: ")
 
-print("Is this your password" + [password])
+    
+    hashed_entered_password = hash_password(entered_password)
+    hashed_reentered_password = hash_password(reentered_password)
+
+  
+    if hashed_entered_password == hashed_reentered_password:
+        print("Passwords match.")
+    else:
+        print("Passwords do not match.")
+
+# Get user details
+name, surname, email = details()
+
+# Print user details
+print("Name:", name)
+print("Surname:", surname)
+print("Email:", email)
+
+# Access the system
+access()
