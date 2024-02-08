ccdb9444e64c0c1caf0fbf385bf2f06fd7776215
“Cardinalempath” <cardinaljamez@gmail.com>
2024-02-08 14:23:58 +0200

    testing

---
+++ run.py
@@ -0,0 +1,12 @@
+def details():
+    input("Enter your name")
+    input("Enter your Surname")
+    input("Enter your email")
+
+print(details)
+
+def access():
+    input("Enter password")
+password =  input("Re- Enter password")
+
+print("Is this your password" + [password])
